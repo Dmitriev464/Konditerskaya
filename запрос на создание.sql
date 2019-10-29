8964a36d8760a296641b84c6336b5ffdf3a413cf
YourName <YourMail@gmail.com>
2019-10-29 15:06:44 +0300

    Sql_datatypes(1)

--- запрос на создание.sql
+++ запрос на создание.sql
@@ -5,72 +5,72 @@ create table [Заказ](
 [Номер] int not null,
 [Дата] datetime not null,
 [Наименование заказа] varchar(max) not null,
-[Изделие] varchar(50) not null,
+[Изделие] varchar(max) not null,
 [Заказчик] varchar(max) not null,
 [Ответственный менеджер] varchar(max) null,
-[Стоимость] smallint null,
+[Стоимость] int null,
 [Плановая дата завершения] datetime null,
 [Примеры работ] image null
 )
 
 create table [Пользователь](
-[Логин] int not null,
-[Пароль] datetime not null,
+[Логин] varchar(max) not null,
+[Пароль] varchar(max) not null,
 [Роль] varchar(max) not null,
-[ФИО] varchar(50) null,
-[Фото] varchar(max) null
+[ФИО] varchar(max) null,
+[Фото] image null
 )
 
 create table [Изделие](
-[Наименование] int not null Primary key,
-[Размеры] datetime not null
+[Наименование] varchar(max) not null Primary key,
+[Размеры] float not null
 )
 
 create table [Спецификация полуфабрикаты](
-[Изделие] int not null,
-[Полуфабрикат] datetime not null,
-[Количество] varchar(max) not null
+[Изделие] varchar(max) not null,
+[Полуфабрикат] varchar(max) not null,
+[Количество] int not null
 )
 
 create table [Тип оборудования](
-[Наименование] int not null Primary key
+[Наименование] varchar(max) not null Primary key
 )
 
 create table [Оборудование](
 [Маркировка] int not null,
-[Тип оборудования] datetime not null,
+[Тип оборудования] varchar(max) not null,
 [Характеристики] varchar(max) null
 )
 
 create table [Спецификация операции](
-[Изделие] int not null,
-[Операция] datetime not null,
-[Порядковый номер] varchar(max) not null,
-[Тип оборудования] varchar(50) null,
-[Время на операцию] varchar(max) not null
+[Изделие] varchar(max) not null,
+[Операция] varchar(max) not null,
+[Порядковый номер] int not null,
+[Тип оборудования] varchar(max) null,
+[Время на операцию] nvarchar(max) not null
 )
 
 create table [Спецификация украшения для торта](
-[Изделие] int not null,
-[Урашение для торта] datetime not null,
-[Количесвто] varchar(max) not null
+[Изделие] varchar(max) not null,
+[Урашение для торта] varchar(max) not null,
+[Количесвто] int not null
 )
 
 create table [Спецификация ингредиенты](
-[Изделие] int not null,
-[Ингредиент] datetime not null,
-[Количество] varchar(max) not null
+[Изделие] varchar(max) not null,
+[Ингредиент] varchar(max) not null,
+[Количество] int not null
 )
 
 create table [Ингредиент](
 [Артикул] int not null,
-[Наименование] datetime not null,
+[Наименование] varchar(max) not null,
 [Единица измерения] varchar(max) not null,
-[Количество] varchar(50) not null,
+[Количество] int not null,
 [Основной поставщик] varchar(max) null,
-[Изображение] varchar(max) null,
+[Изображение] image null,
 [Тип ингредиента] varchar(max) not null,
-[Закупочная цена] varchar(max) not null,
+[Закупочная цена] int not null,
 [ГОСТ] varchar(max) null,
 [Фасовка] varchar(max) null,
 [Характеристика] varchar(max) null
@@ -78,20 +78,20 @@ create table [Ингредиент](
 
 create table [Украшение для торта](
 [Артикул] int not null,
-[Наименование] datetime not null,
+[Наименование] varchar(max) not null,
 [Единица измерения] varchar(max) not null,
-[Количество] varchar(50) not null,
+[Количество] int not null,
 [Основной поставщик] varchar(max) null,
-[Изображение] varchar(max)  null,
+[Изображение] image  null,
 [Тип украшения для торта] varchar(max) not null,
-[Закупочная цена] varchar(max) not null,
-[Вес] varchar(max) not null
+[Закупочная цена] int not null,
+[Вес] float not null
 )
 
 create table [Поставщик](
-[Наименование] int not null Primary key,
-[Адрес] datetime null,
-[Срок доставки] int not null
+[Наименование] varchar(max) not null Primary key,
+[Адрес] varchar(max) null,
+[Срок доставки] nvarchar(max) not null
 )
 
 alter table [Украшение для торта] WITH CHECK ADD FOREIGN KEY ([Основной поставщик]) REFERENCES [Поставщик] ([Наименование]);
