4d9395dd552e8f055e8103878bca34f052357e77
YourName <YourMail@gmail.com>
2019-10-30 09:54:10 +0300

    Sql_datatype(2)

--- запрос на создание.sql
+++ запрос на создание.sql
@@ -4,94 +4,95 @@ use Dmitriev_464_kondit
 create table [Заказ](
 [Номер] int not null,
 [Дата] datetime not null,
-[Наименование заказа] varchar(max) not null,
-[Изделие] varchar(max) not null,
-[Заказчик] varchar(max) not null,
-[Ответственный менеджер] varchar(max) null,
+[Наименование заказа] nvarchar(250) not null,
+[Изделие] nvarchar(250) not null,
+[Заказчик] nvarchar(250) not null,
+[Ответственный менеджер] nvarchar(250) null,
 [Стоимость] int null,
 [Плановая дата завершения] datetime null,
 [Примеры работ] image null
 )
 
 create table [Пользователь](
-[Логин] varchar(max) not null,
-[Пароль] varchar(max) not null,
-[Роль] varchar(max) not null,
-[ФИО] varchar(max) null,
+[Логин] nvarchar(100) not null,
+[Пароль] nvarchar(100) not null,
+[Роль] nvarchar(250) not null,
+[ФИО] nvarchar(250) null,
 [Фото] image null
 )
 
 create table [Изделие](
-[Наименование] varchar(max) not null Primary key,
+[Наименование] nvarchar(250) not null,
 [Размеры] float not null
 )
 
 create table [Спецификация полуфабрикаты](
-[Изделие] varchar(max) not null,
-[Полуфабрикат] varchar(max) not null,
+[Изделие] nvarchar(250) not null,
+[Полуфабрикат]nvarchar(250) not null,
 [Количество] int not null
 )
 
 create table [Тип оборудования](
-[Наименование] varchar(max) not null Primary key
+[Наименование] nvarchar(250) not null
 )
 
 create table [Оборудование](
 [Маркировка] int not null,
-[Тип оборудования] varchar(max) not null,
-[Характеристики] varchar(max) null
+[Тип оборудования] nvarchar(250) not null,
+[Характеристики] nvarchar(1000) null
 )
 
 create table [Спецификация операции](
-[Изделие] varchar(max) not null,
-[Операция] varchar(max) not null,
+[Изделие] nvarchar(250) not null,
+[Операция] nvarchar(250) not null,
 [Порядковый номер] int not null,
-[Тип оборудования] varchar(max) null,
-[Время на операцию] nvarchar(max) not null
+[Тип оборудования] nvarchar(250) null,
+[Время на операцию] nvarchar(250) not null
 )
 
 create table [Спецификация украшения для торта](
-[Изделие] varchar(max) not null,
-[Урашение для торта] varchar(max) not null,
+[Изделие] nvarchar(250) not null,
+[Урашение для торта] nvarchar(250) not null,
 [Количесвто] int not null
 )
 
 create table [Спецификация ингредиенты](
-[Изделие] varchar(max) not null,
-[Ингредиент] varchar(max) not null,
+[Изделие] nvarchar(250) not null,
+[Ингредиент] nvarchar(250) not null,
 [Количество] int not null
 )
 
 create table [Ингредиент](
 [Артикул] int not null,
-[Наименование] varchar(max) not null,
-[Единица измерения] varchar(max) not null,
+[Наименование] nvarchar(250) not null,
+[Единица измерения] nvarchar(250) not null,
 [Количество] int not null,
-[Основной поставщик] varchar(max) null,
+[Основной поставщик] nvarchar(250) null,
 [Изображение] image null,
-[Тип ингредиента] varchar(max) not null,
+[Тип ингредиента] nvarchar(250) not null,
 [Закупочная цена] int not null,
-[ГОСТ] varchar(max) null,
-[Фасовка] varchar(max) null,
-[Характеристика] varchar(max) null
+[ГОСТ] nvarchar(250) null,
+[Фасовка] nvarchar(250) null,
+[Характеристика] nvarchar(250) null
 )
 
 create table [Украшение для торта](
 [Артикул] int not null,
-[Наименование] varchar(max) not null,
-[Единица измерения] varchar(max) not null,
+[Наименование] nvarchar(250) not null,
+[Единица измерения] nvarchar(250) not null,
 [Количество] int not null,
-[Основной поставщик] varchar(max) null,
+[Основной поставщик] nvarchar(250) null,
 [Изображение] image  null,
-[Тип украшения для торта] varchar(max) not null,
+[Тип украшения для торта] nvarchar(250) not null,
 [Закупочная цена] int not null,
 [Вес] float not null
 )
 
 create table [Поставщик](
-[Наименование] varchar(max) not null Primary key,
-[Адрес] varchar(max) null,
-[Срок доставки] nvarchar(max) not null
+[Наименование] nvarchar(250) not null ,
+[Адрес] nvarchar(250) null,
+[Срок доставки] nvarchar(250) not null
 )
 
-alter table [Украшение для торта] WITH CHECK ADD FOREIGN KEY ([Основной поставщик]) REFERENCES [Поставщик] ([Наименование]);
+drop table [Заказ],[Ингредиент],[Изделие],[Оборудование],[Пользователь],[Поставщик],[Спецификация ингредиенты],[Спецификация операции],[Спецификация полуфабрикаты],[Спецификация украшения для торта],[Тип оборудования],[Украшение для торта]
+--alter table [Украшение для торта] WITH CHECK ADD FOREIGN KEY ([Основной поставщик]) REFERENCES [Поставщик] ([Наименование]);
