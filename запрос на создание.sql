bdd70d84a6cad14bb7ca7459cf9fec0695ae0b3e
YourName <YourMail@gmail.com>
2019-11-05 14:36:35 +0300

    changed nvarchar from 250 to 150

--- запрос на создание.sql
+++ запрос на создание.sql
@@ -4,10 +4,10 @@ use Dmitriev_464_kondit
 create table [Заказ](
 [Номер] int not null,
 [Дата] datetime not null,
-[Наименование заказа] nvarchar(250) not null,
-[Изделие] nvarchar(250) not null,
-[Заказчик] nvarchar(250) not null,
-[Ответственный менеджер] nvarchar(250) null,
+[Наименование заказа] nvarchar(150) not null,
+[Изделие] nvarchar(150) not null,
+[Заказчик] nvarchar(150) not null,
+[Ответственный менеджер] nvarchar(150) null,
 [Стоимость] int null,
 [Плановая дата завершения] datetime null,
 [Примеры работ] image null
@@ -16,82 +16,82 @@ create table [Заказ](
 create table [Пользователь](
 [Логин] nvarchar(100) not null,
 [Пароль] nvarchar(100) not null,
-[Роль] nvarchar(250) not null,
-[ФИО] nvarchar(250) null,
+[Роль] nvarchar(150) not null,
+[ФИО] nvarchar(150) null,
 [Фото] image null
 )
 
 create table [Изделие](
-[Наименование] nvarchar(250) not null,
+[Наименование] nvarchar(150) not null,
 [Размеры] float not null
 )
 
 create table [Спецификация полуфабрикаты](
-[Изделие] nvarchar(250) not null,
-[Полуфабрикат]nvarchar(250) not null,
+[Изделие] nvarchar(150) not null,
+[Полуфабрикат]nvarchar(150) not null,
 [Количество] int not null
 )
 
 create table [Тип оборудования](
-[Наименование] nvarchar(250) not null
+[Наименование] nvarchar(150) not null
 )
 
 create table [Оборудование](
 [Маркировка] int not null,
-[Тип оборудования] nvarchar(250) not null,
+[Тип оборудования] nvarchar(150) not null,
 [Характеристики] nvarchar(1000) null
 )
 
 create table [Спецификация операции](
-[Изделие] nvarchar(250) not null,
+[Изделие] nvarchar(150) not null,
 [Операция] nvarchar(100) not null,
 [Порядковый номер] int not null,
-[Тип оборудования] nvarchar(250) null,
-[Время на операцию] nvarchar(250) not null
+[Тип оборудования] nvarchar(150) null,
+[Время на операцию] nvarchar(150) not null
 )
 
 create table [Спецификация украшения для торта](
-[Изделие] nvarchar(250) not null,
+[Изделие] nvarchar(150) not null,
 [Урашение для торта] int not null,
 [Количесвто] int not null
 )
 
 create table [Спецификация ингредиенты](
-[Изделие] nvarchar(250) not null,
+[Изделие] nvarchar(150) not null,
 [Ингредиент] int not null,
 [Количество] int not null
 )
 
 create table [Ингредиент](
 [Артикул] int not null,
-[Наименование] nvarchar(250) not null,
-[Единица измерения] nvarchar(250) not null,
+[Наименование] nvarchar(150) not null,
+[Единица измерения] nvarchar(150) not null,
 [Количество] int not null,
-[Основной поставщик] nvarchar(250) null,
+[Основной поставщик] nvarchar(150) null,
 [Изображение] image null,
-[Тип ингредиента] nvarchar(250) not null,
+[Тип ингредиента] nvarchar(150) not null,
 [Закупочная цена] int not null,
-[ГОСТ] nvarchar(250) null,
-[Фасовка] nvarchar(250) null,
-[Характеристика] nvarchar(250) null
+[ГОСТ] nvarchar(150) null,
+[Фасовка] nvarchar(150) null,
+[Характеристика] nvarchar(150) null
 )
 
 create table [Украшение для торта](
 [Артикул] int not null,
-[Наименование] nvarchar(250) not null,
-[Единица измерения] nvarchar(250) not null,
+[Наименование] nvarchar(150) not null,
+[Единица измерения] nvarchar(150) not null,
 [Количество] int not null,
-[Основной поставщик] nvarchar(250) null,
+[Основной поставщик] nvarchar(150) null,
 [Изображение] image  null,
-[Тип украшения для торта] nvarchar(250) not null,
+[Тип украшения для торта] nvarchar(150) not null,
 [Закупочная цена] int not null,
 [Вес] float not null
 )
 
 create table [Поставщик](
-[Наименование] nvarchar(250) not null ,
-[Адрес] nvarchar(250) null,
-[Срок доставки] nvarchar(250) not null
+[Наименование] nvarchar(150) not null ,
+[Адрес] nvarchar(150) null,
+[Срок доставки] nvarchar(150) not null
 )
 
 drop table [Заказ],[Ингредиент],[Изделие],[Оборудование],[Пользователь],[Поставщик],[Спецификация ингредиенты],[Спецификация операции],[Спецификация полуфабрикаты],[Спецификация украшения для торта],[Тип оборудования],[Украшение для торта]
